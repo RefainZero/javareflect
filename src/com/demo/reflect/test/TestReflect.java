dd50290eb2ca265a03b8783b5573e865fb15f472
RefainZero <1399811201@qq.com>
2017-04-11 10:54:23 +0800

    加上必要注释

--- src/com/demo/reflect/test/TestReflect.java
+++ src/com/demo/reflect/test/TestReflect.java
@@ -24,34 +24,35 @@ public class TestReflect {
 		System.out.println(query(f3));
 	}
 
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public String query(Object f) {
 		StringBuffer sb = new StringBuffer();
 		// TODO Auto-generated method stub
-//		获取该类类型
-		Class c = f.getClass();	 
-//		获取数据库表名
+		// 获取该类类型
+		Class c = f.getClass();
+		// 获取数据库表名
 		boolean isTable = c.isAnnotationPresent(Table.class);
 		if (!isTable) {
 			return null;
 		}
 		Table t = (Table) c.getAnnotation(Table.class);
-		String tableName=t.value();
+		String tableName = t.value();
 		sb.append("select * from ").append(tableName).append(" where 1=1 ");
-//		获取所有字段名
-		Object invoke=null;
+		// 获取所有字段名
+		Object invoke = null;
 		Field[] fields = c.getDeclaredFields();
 		for (Field field : fields) {
-			String fieldName=field.getName();
-//			获取字段值
+			String fieldName = field.getName();
+			// 获取字段值
 			try { 
 				String methodName="get"+fieldName.substring(0,1).toUpperCase()+fieldName.substring(1);
 				Method method = c.getMethod(methodName);
 				invoke = method.invoke(f);
-//				拼接sql
-				if (invoke==null||invoke instanceof Integer&& ((Integer)(invoke)==0)) {
+				// 拼接sql
+				if (invoke == null || invoke instanceof Integer&& ((Integer) (invoke) == 0)) {
 					continue;
 				}
+				// 多个邮箱账号情况
 				if (invoke instanceof String) {
 					if ((boolean)(invoke.toString()).contains(",")) {
 						String[] value = ((String) invoke.toString()).split(",");
@@ -60,7 +61,7 @@ public class TestReflect {
 						for (String values : value) {
 							sb.append("'").append(values).append("'").append(",");
 						}
-//						删掉最后的逗号
+						// 删掉最后的逗号
 						sb.deleteCharAt(sb.length()-1);
 						sb.append(')');
 					}else {
